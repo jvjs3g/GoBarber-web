9a882b3da43650c5fa213f2b03426fdfba2944f6
joao vitor <joao3klb@hotmail.com>
2020-10-31 20:03:09 -0300

    dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () =>{
@@ -26,6 +26,32 @@ const Dashboard: React.FC = () =>{
         <button type="button" onClick={signOut}> <FiPower/></button>
       </HeaderContent>
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+        <span>hoje</span>
+        <span>dia 06</span>
+        <span>segunda-feira</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir</strong>
+          <div>
+            <img src="https://avatars3.githubusercontent.com/u/44537126?s=460&u=17a9a5508a165e1cc0c1b4b03cccae3791d5b4bd&v=4" alt="Joao vitor de jesus silva"/>
+
+            <strong>Joao vitor de jesus silva</strong>
+            <span>
+              <FiClock/>
+              08:00
+            </span>
+          </div>
+
+        </NextAppointment>
+      </Schedule>
+      <Calendar/>
+    </Content>
   </Container>
   );
 };
