283777589b3acefe0e01ec1aea3d4f88f59cffd1
joao vitor <joao3klb@hotmail.com>
2020-10-30 21:49:19 -0300

    dashBoad

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 
@@ -13,7 +19,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextDate {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
