1e1a674b61c0d3c98f51df3e1a67433908ac9651
joao vitor <joao3klb@hotmail.com>
2020-11-10 20:17:26 -0300

    Página de perfil

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -4,6 +4,7 @@ import api from '../services/api';
 interface User {
   id: string;
   name: string;
+  email: string;
   avatar_url: string;
 }
 
@@ -12,30 +13,26 @@ interface AuthState {
   user: User;
 }
 
-
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
-interface AuthContextDate {
+interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
-export const AuthContext = createContext<AuthContextDate>({} as AuthContextDate);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-
   const [data, setData] = useState<AuthState>(() => {
-
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
 
-    if(token && user){
-
-
+    if (token && user) {
       api.defaults.headers.authorization = `Bearer ${token}`;
 
       return { token, user: JSON.parse(user) };
@@ -44,8 +41,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     return {} as AuthState;
   });
 
-  const signIn = useCallback( async ({ email, password }) => {
-
+  const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
@@ -57,26 +53,39 @@ export const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
     api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
   const signOut = useCallback(() => {
-
     localStorage.removeItem('@GoBarber:token');
     localStorage.removeItem('@GoBarber:user');
 
     setData({} as AuthState);
-
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
 
-export function useAuth(): AuthContextDate {
+export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
@@ -84,4 +93,4 @@ export function useAuth(): AuthContextDate {
   }
 
   return context;
-};
+}
---
+++ src/styles/variables.ts
@@ -0,0 +1,22 @@
+import { rgba } from "polished";
+
+export const colors = {
+  primary: '#ff9000',
+  secondary: '#312e38',
+  blue: '#3172b7',
+  dark: '#232129',
+  dark2: '#28262e',
+  green: '#2e656a',
+  lightGrey: '#999591',
+  grey: '#666360',
+  darkGrey: '#3e3b47',
+  light: '#ffffff',
+  light2: '#f4ede8',
+  lightestBlue: '#ebf8ff',
+  lightestGreen: '#e6fffa',
+  lightestRed: '#fddede',
+  red: '#c53030',
+  blackHighTransparence: rgba(0, 0, 0, 0.2),
+};
+
+export const defaultTransition = '300ms ease-in-out';
