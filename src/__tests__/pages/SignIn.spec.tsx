951019be42fed1f4b6eb7ccbb21f59c56f475c8c
joao vitor <joao3klb@hotmail.com>
2020-12-04 21:10:53 -0300

    Mock do hook de autenticação

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent,  } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
+
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
-describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+jest.mock('../../hooks/auth',() =>{
+  return{
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  }
+})
 
-    debug();
+describe('SignIn Page', () => {
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { valeu: 'joao3kln@hotmil.com' } });
+    fireEvent.change(passwordField, { target: { valeu: '123456' } });
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+
   });
 });
 //
